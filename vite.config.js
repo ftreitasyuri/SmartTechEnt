4ea39c371be707d0df72c34cbef1de944db58ebd
ftreitasyuri <yuriftq@gmail.com>
2025-04-24 21:43:57 -0300

    Uploada: Configuração para fazer o post de contato via api

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SiteSmartTechEnterprise/',
+  base: '/SmartTechEnt/',
   plugins: [
     vue(),
     vueDevTools(),
